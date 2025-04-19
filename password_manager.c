5389b59c516ddb804c05422d15d6265ae415012d
Nicholas Harris <harrisn@my.erau.edu>
2025-04-19 16:59:37 -0700

    Update password_manager.c

--- password_manager.c
+++ password_manager.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include "password_manager.h"
 
+#define ELEMENT_LENGTH 10
+
 char key[16] = "s0ftw4r3secur1ty"; 
 char iv[16] = "superdupersecret";
 
@@ -130,6 +132,50 @@ void showPassword() {
        - Check if that user already has a .pm directory (Check the exit code from the "ls /home/[username]/.pm/" command?)
        - If not, make the directory. If yes, open the creds.enc file within that directory (see below)
     */
+    struct list creds[ELEMENT_LENGTH]; // An array that holds 10 struct list elements 
+    int i = 0; // A counter to loop through elements of the array 
+    int arr_len = 0; 
+    
+    FILE *file = fopen("creds.enc", "rb");
+    if (!file) {
+        perror("Failed to open file");
+        return;
+    }
+
+    int site_len, username_len, ciphertext_len, plaintext_len;
+    char site[64], username[64], ciphertext[512], plaintext[512];
+
+    while (fread(&site_len, sizeof(int), 1, file) == 1) {
+        fread(site, 1, site_len, file);
+        site[site_len] = '\0';
+
+        fread(&username_len, sizeof(int), 1, file);
+        fread(username, 1, username_len, file);
+        username[username_len] = '\0';
+
+        fread(&ciphertext_len, sizeof(int), 1, file);
+        fread(ciphertext, 1, ciphertext_len, file);
+
+        decryptPassword(ciphertext, plaintext, ciphertext_len, &plaintext_len);
+        plaintext[plaintext_len] = '\0';
+        strcpy(creds[i].site, site); 
+        strcpy(creds[i].user, username); 
+        strcpy(creds[i].pass, plaintext);
+        i++;
+    }
+    printf("\nThe current saved passwords are:\n"); 
+    arr_len = i;
+    printArray(creds, arr_len); 
+
+    fclose(file);
+}
+
+void deletePassword() {
+    int num;
+    struct list creds[ELEMENT_LENGTH];
+    int i = 0, arr_len = 0;
+
+    // Open the file and load all entries (same logic as in showPassword)
     FILE *file = fopen("creds.enc", "rb");
     if (!file) {
         perror("Failed to open file");
@@ -153,12 +199,59 @@ void showPassword() {
         decryptPassword(ciphertext, plaintext, ciphertext_len, &plaintext_len);
         plaintext[plaintext_len] = '\0';
 
-        printf("Site: %s, Username: %s, Password: %s\n", site, username, plaintext);
+        strcpy(creds[i].site, site);
+        strcpy(creds[i].user, username);
+        strcpy(creds[i].pass, plaintext);
+        i++;
+    }
+    fclose(file);
+    arr_len = i;
+
+    printArray(creds, arr_len);
+
+    printf("Which password would you like to delete? Please only enter the number: ");
+    scanf("%d", &num);
+    getchar(); // Clear newline
+
+    if (num < 1 || num > arr_len) {
+        printf("Invalid entry number.\n");
+        return;
+    }
+
+    // Write back all entries except the one being deleted
+    file = fopen("creds.enc", "wb"); // Overwrite file
+    if (!file) {
+        perror("Failed to open file for writing");
+        return;
+    }
+
+    for (i = 0; i < arr_len; i++) {
+        if (i == num - 1) continue; // Skip the entry to delete
+
+        int site_len = strlen(creds[i].site);
+        int user_len = strlen(creds[i].user);
+        char ciphertext[512];
+        int ciphertext_len = 0;
+
+        encryptPassword(creds[i].pass, ciphertext, &ciphertext_len);
+
+        fwrite(&site_len, sizeof(int), 1, file);
+        fwrite(creds[i].site, 1, site_len, file);
+
+        fwrite(&user_len, sizeof(int), 1, file);
+        fwrite(creds[i].user, 1, user_len, file);
+
+        fwrite(&ciphertext_len, sizeof(int), 1, file);
+        fwrite(ciphertext, 1, ciphertext_len, file);
     }
 
     fclose(file);
+    printf("Entry deleted successfully.\n");
 }
 
-void deletePassword() {
-    // Incomplete
+void printArray(struct list *creds, int arr_len) {
+    int i; 
+    for (i = 0; i < arr_len; i++) {
+    	printf("%d. Site: %s\tUsername: %s\tPassword: %s\n", i+1, creds[i].site, creds[i].user, creds[i].pass); 
+    }    
 }
