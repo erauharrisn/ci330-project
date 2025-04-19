5ef28091bfdbe509528159ac95b27da0f8698760
Nicholas Harris <harrisn@my.erau.edu>
2025-04-19 16:59:56 -0700

    Update password_manager.h

--- password_manager.h
+++ password_manager.h
@@ -1,8 +1,15 @@
 // password_manager.h 
 
+struct list {
+    char user[64]; 
+    char pass[64]; 
+    char site[64];
+};
+
 void encryptPassword(const char* password, char* ciphertext, int* ciphertext_len);
 void decryptPassword(char* ciphertext, char* plaintext, int ciphertext_len, int* plaintext_len);
 void savePassword(const char* site, const char* username, char* ciphertext, int ciphertext_len);
 void deletePassword();
 void showPassword();
 void prompt();
+void printArray(struct list *creds, int arr_len);
