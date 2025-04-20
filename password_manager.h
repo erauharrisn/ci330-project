3c44b408db91c6c88c8d05a8b54a8e25254d811d
Nicholas Harris <harrisn@my.erau.edu>
2025-04-20 15:13:26 -0700

    Update password_manager.h
    Final update

--- password_manager.h
+++ password_manager.h
@@ -1,15 +1,18 @@
 // password_manager.h 
 
+// List struct to hold usernames, passwords, and sites during user input. 
 struct list {
     char user[64]; 
     char pass[64]; 
     char site[64];
 };
 
+// Essential Program Functions 
+void prompt();
+void ensureUserDirectory(); 
+void printArray(struct list *creds, int arr_len);
 void encryptPassword(const char* password, char* ciphertext, int* ciphertext_len);
 void decryptPassword(char* ciphertext, char* plaintext, int ciphertext_len, int* plaintext_len);
 void savePassword(const char* site, const char* username, char* ciphertext, int ciphertext_len);
-void deletePassword();
 void showPassword();
-void prompt();
-void printArray(struct list *creds, int arr_len);
+void deletePassword();
